8401f65a2b38a413d1b58f3cbf77b459f770e22a
Aleksei Semochkin <aleksioprime@gmail.com>
2025-06-26 20:24:12 +0300

    fix periodic broadcast

--- web/back/src/api/v1/devices/ws_background.py
+++ web/back/src/api/v1/devices/ws_background.py
@@ -12,6 +12,10 @@ logger = logging.getLogger(__name__)
 async def periodic_broadcast():
     while True:
         try:
+            if not manager.connections:
+                await asyncio.sleep(1)
+                continue
+
             service = await get_device_service(uow=await get_unit_of_work())
             devices = await service.get_all()
             payload = {
@@ -19,6 +23,7 @@ async def periodic_broadcast():
                 "devices": [DeviceSchema.model_validate(obj).model_dump(mode="json") for obj in devices]
             }
             await manager.broadcast(json.dumps(payload))
+            # logger.info(f"[WS] Отправляем payload размером {len(devices)} устройств")
         except Exception as e:
             logger.error(f"WebSocket periodic broadcast error: {e}")
         await asyncio.sleep(5)
